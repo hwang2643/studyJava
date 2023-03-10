a6c753d7eebf9a95ee5384f7e168c057044f5c71
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-03-10 17:43:05 +0900

    W2_day5

--- src/day7/Q714.java
+++ src/day7/Q714.java
@@ -8,12 +8,11 @@ public class Q714 {
 		System.out.print("입력 : ");
 		int in = sc.nextInt();
 		int n = 1;
-		int n1;
 		for(int i = 1; i<=in; i++) {
 			for(int j = in - i; j>=0; j--) {
 				System.out.print(" ");
 			}
-			n1 = 0;
+			int n1 = 0;
 			for(int j = 1; j<=i; j++) {
 				System.out.print(" " + n);
 				n1+=n;
---
+++ src/day7/Q715.java
@@ -0,0 +1,27 @@
+package day7;
+
+import java.util.Scanner;
+
+public class Q715 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 1; true; i++) {
+			System.out.print("가위 / 바위 / 보 입력 : ");
+			String player = sc.next();
+			String com = new String[] {"가위", "바위", "보"}[(int)(Math.random()*3)];
+			if((player.equals("가위") && com.equals("보")) || (player.equals("바위") && com.equals("가위")) || (player.equals("보") && com.equals("바위"))) {
+				System.out.println("--->플레이어 : " + player + "vs 컴 : " + com + "\n--->플레이어 승!");
+				break;
+			}else if((player.equals("가위") && com.equals("바위")) || (player.equals("바위") && com.equals("보")) || (player.equals("보") && com.equals("가위"))) {
+				System.out.println("--->플레이어 : " + player + "vs 컴 : " + com + "\n--->플레이어 패.");
+				break;
+			}else if((player.equals("가위") && com.equals("가위")) || (player.equals("바위") && com.equals("바위")) || (player.equals("보") && com.equals("보"))) {
+				System.out.println("--->플레이어 : " + player + "vs 컴 : " + com + "\n--->(무승부)");
+				break;
+			}else {
+				System.out.println("잘못 입력하셨습니다.");
+			}
+		}
+	}
+}
